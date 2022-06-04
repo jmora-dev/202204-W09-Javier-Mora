99acb66145278e9da367a1e760d6caf2b56b84f3
jmora-dev <dam2mora@gmail.com>
2022-06-04 14:59:02 +0200

    Change main content

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ const app = document.querySelector<HTMLDivElement>("#app");
 
 if (app) {
   app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
+  <h1>Hi, welcome to Pokémon fan page !!!</h1>
+  <p>Sorry, page under construction</p>
 `;
 }
